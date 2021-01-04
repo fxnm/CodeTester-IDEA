1687de4586b402d6a54f35f10650f7b42a7821db
fxnm <47592197+fxnm@users.noreply.github.com>
2021-01-04 23:39:46 +0100

    Kotlin Version update to 1.4.21-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.4.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.4.21-2" apply false
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.intellij") version "0.6.5"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
