b766e2460b2f538d0324bc84e262b5310134a0f2
fxnm <47592197+fxnm@users.noreply.github.com>
2021-07-13 11:02:12 +0200

    Changed plugin until build version to 212.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.20" apply false
 
     // IntelliJ Gradle Plugin
-    id("org.jetbrains.intellij") version "1.0"
+    id("org.jetbrains.intellij") version "1.1.2"
 
     // IntelliJ Changelog Plugin
     id("org.jetbrains.changelog") version "1.1.2" apply false
